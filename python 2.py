02d2cc1339c169a186b913997bacfd8e2e6bca8d
dodo <dinamicheal82@gmail.com>
2020-04-22 14:03:57 -0400

    adding 2nd assignment

---
+++ python 2.py
@@ -0,0 +1,27 @@
+
+
+age =input('Are you a cigarette addict older than 75 years old: [yes , no]?:').lower()
+
+chronic = input('enter if you have severe chronic disease [yes, no]?:').lower()
+immune = input('enter if your immune system too weak[yes,no]?: ').lower()
+
+if age == 'yes' :
+   age = True
+else:
+  age = False
+
+
+if chronic == 'yes':
+   chronic = True
+else:
+   chronic =False
+
+if  immune == 'yes' :
+    immune = True
+else:
+    immun = False
+
+if age or chronic or immune == True   :
+   print("you're in risky group")
+else:
+   print("you're not in riskey group")
